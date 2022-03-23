e03ff7d37c949edd21e5e0d22bb45dcebc73792f
Muhammad Nasir <muhammadnasir512@gmail.com>
2022-03-23 14:37:21 +0000

    Initial Commit

---
+++ DisneyCharacters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisneyCharacters
+//
+//  Created by Muhammad Nasir Ali on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisneyCharacters/DisneyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyCharactersApp.swift
+//  DisneyCharacters
+//
+//  Created by Muhammad Nasir Ali on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DisneyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
