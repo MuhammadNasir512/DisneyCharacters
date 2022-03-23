8cadfcd35ae88242e0dcec1991b5fb5ec373e4a0
Muhammad Nasir <muhammadnasir512@gmail.com>
2022-03-23 20:44:34 +0000

    Implemented image caching

--- DisneyCharacters/CharactersList/CharacterItemViewModel.swift
+++ DisneyCharacters/CharactersList/CharacterItemViewModel.swift
@@ -14,24 +14,34 @@ extension CharacterItemView {
         let tvShows: [String]
         let videoGames: [String]
         let imageUrlString: String
+        var cache: NSCache<NSString, UIImage>
         
         let nameTitle = "Name:"
         let filmsTitle = "Films:"
         let tvShowsTitle = "TV Shows:"
         let videoGamesTitle = "Video Games:"
 
-        init(charactersModel: CharactersModel, apiHandler: APIHandlerType) {
+        init(
+            charactersModel: CharactersModel,
+            apiHandler: APIHandlerType,
+            cache: NSCache<NSString, UIImage> = NSCache<NSString, UIImage>()
+        ) {
             self.apiHandler = apiHandler
+            self.cache = cache
             id = charactersModel.id
             name = charactersModel.name
             films = charactersModel.films
             tvShows = charactersModel.tvShows
             videoGames = charactersModel.videoGames
             imageUrlString = charactersModel.imageUrl
-            if let image = UIImage(named: "Placeholder") {
+            cache.countLimit = 500
+            
+            if let image = cache.object(forKey: NSString(string: imageUrlString)) {
+                self.image = image
+            } else if let image = UIImage(named: "Placeholder") {
                 self.image = image
             }
-
+            
             isLoading = true
             firstly {
                 apiHandler.getImage(urlString: imageUrlString)
@@ -40,6 +50,7 @@ extension CharacterItemView {
             }.done { [weak self] data in
                 guard let self = self, let image = UIImage(data: data) else { return }
                 self.image = image
+                self.cache.setObject(image, forKey: NSString(string: self.imageUrlString))
             }.catch { _ in }
         }
     }
